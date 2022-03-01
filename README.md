da662d23c32684a3cb9c9012b5dca057faa49367
RaviTejaVattem <ravitejavattem@gmail.com>
2022-03-01 23:02:49 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-cuxnx5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-cuxnx5)
---
+++ index.js
@@ -0,0 +1,14 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+
+
+var cities = [];
+
+function setup() {
+  createCanvas(400, 300)
+}
